7ce9ab1b56a0aac896dedc25ec8cf36e6290609b
Ray Medrano <rmbw74@gmail.com>
2017-12-07 13:55:17 -0600

    made API ket a constant so its not pushed up to github

--- app/apiSearch/controllers/apiSearchCtrl.js
+++ app/apiSearch/controllers/apiSearchCtrl.js
@@ -5,12 +5,12 @@ angular
         $scope.searchString = ""
 
 
-
+        $scope.addMovie = event => console.log(event)
 
         $scope.finder = event => {
             if (event.key === "Enter") {
                 $scope.results = []
-                apiFactory.list($scope.searchString).then(data => {
+                apiFactory.TMDB_list($scope.searchString).then(data => {
                     $scope.results = data[3]
                     console.log("results",$scope.results)
                     $scope.searchString = ""
--- app/apiSearch/factories/apiFactory.js
+++ app/apiSearch/factories/apiFactory.js
@@ -1,16 +1,16 @@
 angular
 .module("codex")
-.factory("apiFactory", function ($http) {
+.factory("apiFactory", function ($http, TMDB_KEY) {
     return Object.create(null, {
         "cache": {
             value: null,
             writable: true
         },
-        "list": {
+        "TMDB_list": {
             value: function (searchString) {
                 return $http({
                     method: "GET",
-                    url: `https://api.themoviedb.org/3/search/movie?api_key=18da23277f663e834d8659e554a6fc68&language=en-US&query=${searchString}&page=1&include_adult=false`
+                    url: `https://api.themoviedb.org/3/search/movie?api_key=${TMDB_KEY}&language=en-US&query=${searchString}&page=1&include_adult=false`
                 }).then(response => {
                     const data = response.data
                     this.cache = Object.keys(data).map(key => {
