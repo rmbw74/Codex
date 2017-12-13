cbac54af7c9e50024f26aa007d86c67b2c307d8b
Ray Medrano <rmbw74@gmail.com>
2017-12-13 13:29:23 -0600

    added broadcast event + removed user email from welcome

--- app/navigation/controllers/NavCtrl.js
+++ app/navigation/controllers/NavCtrl.js
@@ -1,12 +1,15 @@
 angular
 .module("codex").controller("NavCtrl",
-function ($scope, $location, AuthFactory) {
+function ($scope, $location, AuthFactory, $rootScope) {
     /*
     Just a pass-through method to the AuthFactory method of the
     same name.
     */
     $scope.isAuthenticated = () => AuthFactory.isAuthenticated()
 
+    $rootScope.$on("authenticationSuccess", function () {
+        $scope.email = AuthFactory.getUser().email
+    })
     /*
     Unauthenticate the client.
     */
