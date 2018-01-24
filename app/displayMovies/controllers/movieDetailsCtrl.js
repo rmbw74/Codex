7dddce12f35e3e9d655f9f8347dbd82dfe0d2d53
Ray Medrano <rmbw74@gmail.com>
2018-01-24 07:33:49 -0600

    fixed error on master branch

--- app/displayMovies/controllers/movieDetailsCtrl.js
+++ app/displayMovies/controllers/movieDetailsCtrl.js
@@ -1,6 +1,6 @@
 angular
     .module("codex")
-    .controller("movieDetailsCtrl", function ($scope, $routeParams, databaseFactory, $location,$timeout) {
+    .controller("movieDetailsCtrl", function ($scope, $routeParams, databaseFactory, $location, $timeout) {
         $scope.movie = {}
         $scope.formats = {}
         $scope.currentMovieFormats = []
@@ -28,21 +28,20 @@ angular
         //createformats()
         $scope.addFormatToMovie = selectedFormat => {
             //check to see if the current movie is already owned in a particular format (will return -1 if exists)
-            if ($scope.currentMovieFormats.indexOf(selectedFormat) < 0)
-            {
+            if ($scope.currentMovieFormats.indexOf(selectedFormat) < 0) {
                 //if the current movie has not been formated, remove none then replace with new format
-                if($scope.currentMovieFormats[0] === "none"){
+                if ($scope.currentMovieFormats[0] === "none") {
                     $scope.currentMovieFormats.push(selectedFormat)
                     $scope.currentMovieFormats.splice(0, 1)
-                    databaseFactory.replace($scope.movie,$routeParams.movieId)
-                }else {
+                    databaseFactory.replace($scope.movie, $routeParams.movieId)
+                } else {
                     //if movie is already formatted, just add new format to array and then send movie to firebase.
                     $scope.movie.format.push(selectedFormat)
-                    databaseFactory.replace($scope.movie,$routeParams.movieId)
+                    databaseFactory.replace($scope.movie, $routeParams.movieId)
 
                 }
 
-            }else{
+            } else {
                 alert("You aleady own the movie in this format, try again")
             }
         }
@@ -51,24 +50,23 @@ angular
             //find what index that the selected format is in the current movie
             let indexToRemove = $scope.movie.format.indexOf(selectedFormat)
             //if the format exists in the array than splice the format off the array and the array is > 0
-            if ($scope.movie.format.length > 0 && indexToRemove > -1){
-                $scope.movie.format.splice(indexToRemove,1)
+            if ($scope.movie.format.length > 0 && indexToRemove > -1) {
+                $scope.movie.format.splice(indexToRemove, 1)
                 //if all formats are removed from the film, push the "none" format back to the array
-                if ($scope.movie.format.length < 1){
+                if ($scope.movie.format.length < 1) {
                     $scope.movie.format.push("none")
                     //push the  movie back to firebase
                     databaseFactory.replace($scope.movie, $routeParams.movieId)
+                }
                 //push the movie back to firebase
                 databaseFactory.replace($scope.movie, $routeParams.movieId)
             }
-
-
         }
         //this function is called when the user clicks on a movie and will delete the movie from the DB
         //and return the user to the welcome screen
         $scope.removeMovie = () => {
             //call database remove function pass it the movieId
-            databaseFactory.remove($routeParams.movieId,"movies")
+            databaseFactory.remove($routeParams.movieId, "movies")
             //send the user back to the welcome
             $timeout(function () {
                 $location.url("/welcome")
